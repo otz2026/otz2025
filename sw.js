86f880b00fe1ae78e1656a569cd185e70b1fc921
Otz2025 <makolinb@ya.ru>
2025-03-25 20:23:20 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'my-site-cache-v1'; // Можете изменить название кэша (например, "my-app-v1")
+
+// Список файлов для кэширования (укажите свои!)
+const urlsToCache = [
+  '/',              // Главная страница
+  '/index.html',    // Основной HTML
+  '/style.css',    // CSS-файл (если есть)
+  '/script.js',     // JS-файл (если есть)
+  '/img/icon-192x192.png', // Иконка
+  '/img/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache)) // Кэшируем файлы
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request)) // Отдаём из кэша или загружаем
+  );
+});
