4b8197e1472e90fe205cc8a03920753e32f5f60a
Otz2025 <makolinb@ya.ru>
2025-03-25 20:24:31 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -23,3 +23,11 @@ self.addEventListener('fetch', (event) => {
       .then((response) => response || fetch(event.request)) // Отдаём из кэша или загружаем
   );
 });
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js') // Укажите правильный путь, если sw.js не в корне
+      .then(registration => console.log('ServiceWorker зарегистрирован'))
+      .catch(err => console.log('Ошибка ServiceWorker:', err));
+  });
+}
