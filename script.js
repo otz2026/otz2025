7d4a4373f4be881f321f159008337a9989376702
Otz2025 <makolinb@ya.ru>
2025-04-26 12:05:57 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js') // Укажите правильный путь, если sw.js не в корне
-      .then(registration => console.log('ServiceWorker зарегистрирован'))
-      .catch(err => console.log('Ошибка ServiceWorker:', err));
-  });
-}
